86ac1bea9c2a8a4523527e1bca72efc0e7b405d4
Franco Peña Calle <francopc24@gmail.com>
2026-05-20 22:28:09 -0500

    fix: eliminar carpeta duplicada

--- avance de proyecto final 2 - herramientas de desarrollo /src/main/java/com/scholarstay/app/repository/AlojamientoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.scholarstay.app.repository;
-
-import com.scholarstay.app.model.Alojamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlojamientoRepository extends JpaRepository<Alojamiento, Long> {
-}
